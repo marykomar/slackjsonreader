fe4dd118aa8f9692085911a26d0e6a5b01d102ca
marykomar <maria.komar@gmail.com>
2017-03-30 16:44:48 +0300

    Test fixed

--- src/test/java/com/mariakomar/slackjsonreader/MappingServiceJacksonTest.java
+++ src/test/java/com/mariakomar/slackjsonreader/MappingServiceJacksonTest.java
@@ -9,6 +9,7 @@ import org.junit.rules.TemporaryFolder;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
@@ -28,9 +29,29 @@ public class MappingServiceJacksonTest {
     @Before
     public void beforeEachTest() throws IOException {
         ms = new MappingServiceJackson();
-        File tempFolder = testFolder.newFolder();
-        String testDirectory = "/home/maria/tes";
-        ms.setMainDirectoryPath(testDirectory);
+        File tempFolder = testFolder.newFolder("dir", "channel");
+        File tempFile = new File(tempFolder, "test.txt");
+        FileWriter fileWriter = new FileWriter(tempFile);
+        String json = "[\n" +
+                "    {\n" +
+                "        \"user\": \"id1\",\n" +
+                "        \"type\": \"message\",\n" +
+                "        \"subtype\": \"channel_join\",\n" +
+                "        \"text\": \"<user> has joined the channel\",\n" +
+                "        \"ts\": \"1463495007.000002\"\n" +
+                "    },\n" +
+                "    {\n" +
+                "        \"user\": \"id2\",\n" +
+                "        \"type\": \"message\",\n" +
+                "        \"subtype\": \"channel_purpose\",\n" +
+                "        \"text\": \"<user> has joined the channel\",\n" +
+                "        \"ts\": \"1463495007.000003\"\n" +
+                "    }\n" +
+                "]";
+        fileWriter.write(json);
+        fileWriter.flush();
+        fileWriter.close();
+        ms.setMainDirectoryPath(tempFolder.getParentFile().getAbsolutePath());
     }
 
     @Test
