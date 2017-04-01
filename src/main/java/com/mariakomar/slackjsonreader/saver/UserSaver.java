6bbabbb8567ccf7e7533739d9e3d589f3b4409f5
marykomar <maria.komar@gmail.com>
2017-04-01 20:42:07 +0300

    Refactoring of MappingService

--- src/main/java/com/mariakomar/slackjsonreader/saver/UserSaver.java
+++ src/main/java/com/mariakomar/slackjsonreader/saver/UserSaver.java
@@ -71,8 +71,6 @@ public class UserSaver {
      * Current path : "/home/maria/!slack/users.txt"
      */
     public void saveUsersJsonToFile(PrintWriter out) {
-//        String path = "/home/maria/!slack/users.txt";
-//        try (PrintWriter out = new PrintWriter(path)) {
         out.println("[");
         for (int i = 0; i < getUsersInfoFromSlack().size() - 1; i++) {
             out.print(getUsersInfoFromSlack().get(i)
@@ -80,9 +78,6 @@ public class UserSaver {
         }
         out.print(getUsersInfoFromSlack().get(getUsersInfoFromSlack().size() - 1));
         out.println("]");
-//        } catch (IOException e) {
-//            logger.warn("File users.txt not filled", e);
-//        }
         logger.info("Users data written to output");
     }
 
@@ -91,10 +86,8 @@ public class UserSaver {
      * @return list containing all users from specified file
      * Now saving to "/home/maria/!slack/users.txt"
      */
-    // TODO move to different class?
     public List<SlackUser> getUsersFromFile(InputStream inputStream) {
         List<SlackUser> users = new ArrayList<>();
-//        File usersFile = new File("/home/maria/!slack/users.txt");
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             ArrayNode arrNode = objectMapper.readValue(inputStream, ArrayNode.class);
@@ -121,7 +114,6 @@ public class UserSaver {
      * Current path "/home/maria/!slack/avatars/"
      */
     public void saveAvatars(String path, List<SlackUser> users) {
-        //String path = "/home/maria/!slack/avatars/";
         for (SlackUser user : users) {
             String url = user.getAvatar();
             String name = user.getId();
